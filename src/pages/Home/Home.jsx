32c0fba1dd9bd5dc51e1e9c0f0cfa4b52db0bd7a
agasefamircan <agasefemircanov@gmail.com>
2023-02-04 13:30:58 +0400

    vrcl

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,31 +1,29 @@
-// import React,{useState} from 'react'
-import Promo from '../../components/Promo/Promo'
-import Head from '../../components/Header/Head'
-import Discount from '../../components/Discount'
-import CardSlider from '../../components/CardSlider/CardSlider'
-import Info from '../../components/Info'
-import Offer from '../../components/Offer'
-import TopSellers from '../../components/Sellers/TopSellers'
-import Countdown from '../../components/Countdown'
-import Feedback from '../../components/Feedback/Feedback'
-import Company from '../../components/Company'
-import Footer from '../../components/Footer/Footer'
+import Promo from "../../components/Promo/Promo";
+import Head from "../../components/Header/Head";
+import Discount from "../../components/Discount";
+import CardSlider from "../../components/CardSlider/CardSlider";
+import Info from "../../components/Info";
+import Offer from "../../components/Offer";
+import TopSellers from "../../components/Sellers/TopSellers";
+import Countdown from "../../components/Countdown";
+import Feedback from "../../components/Feedback/Feedback";
+import Company from "../../components/Company";
+import Footer from "../../components/Footer/Footer";
 const Home = () => {
   return (
     <div className="Home">
-      <Promo/>
-      <Head/>
-      <Discount/>
-      <CardSlider/> 
-      <Info/>
-      <Offer/>
-      <TopSellers/>
-      <Countdown/>
-      <Feedback/>
-      <Company/>
-      <Footer/>
+      <Promo />
+      <Head />
+      <Discount />
+      <CardSlider />
+      <Info />
+      <Offer />
+      <TopSellers />
+      <Countdown />
+      <Feedback />
+      <Company />
+      <Footer />
     </div>
-  )
-}
-
-export default Home
+  );
+};
+export default Home;
